0e3d5fac916508b4c08a4f6200f94cff2df1b44d
Hussain <hussain.alialkhafaji567@gmail.com>
2026-03-26 23:04:26 -0400

    Move component files into a directory called blocks

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,4 +16,9 @@ export default defineConfig({
       },
     }), tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
 })
