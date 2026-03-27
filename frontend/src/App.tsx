06e2857917f20532a82555ce3c296a7254cecc83
Hussain <hussain.alialkhafaji567@gmail.com>
2026-03-26 23:35:10 -0400

    Test add ShadCN button component to the website

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button.tsx'
+
 function App() {
   return (
     <>
       <div className="font-patrick text-3xl">Anatomy Explorer App</div>
+      <Button>Click me!</Button>
     </>
   )
 }
