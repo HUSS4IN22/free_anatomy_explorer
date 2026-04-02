f44d140b0221ae627958373396fd625c3313c8f9
Hussain <hussain.alialkhafaji567@gmail.com>
2026-04-02 19:02:02 -0400

    Configure main.tsx  to use Data mode of React Router instead of declarative

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,16 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router'
+// import { BrowserRouter } from 'react-router' <- Declarative React Router
+import { createBrowserRouter } from "react-router"
+import { RouterProvider } from "react-router/dom"
 import { store } from '@/store/store.ts'
 import App from './App.tsx'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+])
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 )
