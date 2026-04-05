4c080c099bc8572446c7bad9301b74cd3ec4d030
Hussain <hussain.alialkhafaji567@gmail.com>
2026-04-04 22:19:50 -0400

    remove placeholder items in mainpage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,9 @@
-import { Button } from '@/components/ui/button.tsx'
 import { Navbar } from '@/components/ui/Nav-bar.tsx'
 
 function App() {
   return (
     <>
       <Navbar />
-      <div className="font-patrick text-3xl">Anatomy Explorer App</div>
-      <Button>Click me!</Button>
     </>
   )
 }
