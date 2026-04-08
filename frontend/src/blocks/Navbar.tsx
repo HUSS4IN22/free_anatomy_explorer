4fda9d0afab15c351756855088784875df3c8a69
Hussain <hussain.alialkhafaji567@gmail.com>
2026-04-07 21:31:28 -0400

    Add ShadCN menu component into block

--- frontend/src/blocks/Navbar.tsx
+++ frontend/src/blocks/Navbar.tsx
@@ -1,9 +1,23 @@
 //restart Nav
 import { Link } from "react-router";
-
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu"
 
 export default function Navbar() {
   return <nav>
-    <Link />
+    <Link to="/" />
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Item 1</NavigationMenuTrigger>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
   </nav>
 }
