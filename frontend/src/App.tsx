cd02ee3907b29dff3e260f9c86ce0bbb587ed990
Hussain <hussain.alialkhafaji567@gmail.com>
2026-04-08 21:49:46 -0400

    Test Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import { Button as Button } from "@/components/ui/button"
+import Navbar from "./blocks/Navbar"
 
 function App() {
   return (
     <>
+      <Navbar />
       <Button>Click Me!</Button>
     </>
   )
