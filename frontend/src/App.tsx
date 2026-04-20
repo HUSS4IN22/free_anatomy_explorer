176b0904393024b8578a5d7471e551ebd0dd6c76
Hussain <hussain.alialkhafaji567@gmail.com>
2026-04-19 23:01:32 -0400

    fix some global styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
-import { Button as Button } from "@/components/ui/button"
 import Navbar from "./blocks/Navbar"
 
 function App() {
   return (
     <>
       <Navbar />
-      <Button>Click Me!</Button>
     </>
   )
 }
