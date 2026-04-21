b7d83e787c7bb7d63b35c45cc57007c304f2f0e2
Hussain <hussain.alialkhafaji567@gmail.com>
2026-04-20 22:52:04 -0400

    resize input group

--- frontend/src/blocks/MainSearch.tsx
+++ frontend/src/blocks/MainSearch.tsx
@@ -1,7 +1,7 @@
 import { InputGroup, InputGroupAddon, InputGroupButton, InputGroupInput } from "@/components/ui/input-group"
 
 export default function MainSearch() {
-  return <div>
+  return <div className="flex flex-1 items-center justify-center max-w-2xl">
     <InputGroup>
       <InputGroupAddon align="inline-start">icon</InputGroupAddon>
       <InputGroupInput placeholder="Search a body structure..." className="text-sm font-patrick" />
