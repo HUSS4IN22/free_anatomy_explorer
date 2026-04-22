3e54fb85e5055038f87afa755bac0f1f2717bdf3
Hussain <hussain.alialkhafaji567@gmail.com>
2026-04-21 23:27:48 -0400

    fix CSS margin issue

--- frontend/src/blocks/MainSearch.tsx
+++ frontend/src/blocks/MainSearch.tsx
@@ -1,13 +1,17 @@
 import { InputGroup, InputGroupAddon, InputGroupButton, InputGroupInput } from "@/components/ui/input-group"
 
 export default function MainSearch() {
-  return <div>
-    <h1>What are we studying today?</h1>
-    <p>Look up a bone, muscle, or structure you want to explore</p>
-    <InputGroup>
-      <InputGroupAddon align="inline-start">icon</InputGroupAddon>
-      <InputGroupInput placeholder="Search a body structure..." className="text-sm font-patrick" />
-      <InputGroupButton>Enter</InputGroupButton>
-    </InputGroup>
-  </div>
+  return (
+    <div className="flex flex-col min-h-screen items-center justify-center gap-4 px-6 bg-amber-800">
+      <div className="my-auto mx-auto">
+        <h1>What are we studying today?</h1>
+        <p>Look up a bone, muscle, or structure you want to explore</p>
+        <InputGroup>
+          <InputGroupAddon align="inline-start">icon</InputGroupAddon>
+          <InputGroupInput placeholder="Search a body structure..." className="text-sm font-patrick" />
+          <InputGroupButton>Enter</InputGroupButton>
+        </InputGroup>
+      </div>
+    </div>
+  )
 }
--- frontend/src/blocks/Navbar.tsx
+++ frontend/src/blocks/Navbar.tsx
@@ -15,7 +15,7 @@ import { Button } from "@/components/ui/button"
 
 export default function Navbar() {
   return (
-    <header className="sticky top-4 z-50 mx-auto mt-4 max-w-[95%] rounded-2xl">
+    <header className="fixed top-4 z-50 left-1/2 -translate-x-1/2 w-full max-w-[95%] rounded-2xl bg-background/80 backdrop-blur-lg border border-border">
       <div className="flex h-16 items-center justify-between gap-4 px-6">
         <div className="flex flex-1 items-center justify-start">
           <Link to="/" className="flex items-center gap-2 transition-opacity hover:opacity-80">
