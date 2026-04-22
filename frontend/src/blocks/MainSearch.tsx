0cfd844365b055c8d1e54c458ecf59b85f82684a
Hussain <hussain.alialkhafaji567@gmail.com>
2026-04-21 23:30:29 -0400

    remove navbar backdrop

--- frontend/src/blocks/MainSearch.tsx
+++ frontend/src/blocks/MainSearch.tsx
@@ -2,7 +2,7 @@ import { InputGroup, InputGroupAddon, InputGroupButton, InputGroupInput } from "
 
 export default function MainSearch() {
   return (
-    <div className="flex flex-col min-h-screen items-center justify-center gap-4 px-6 bg-amber-800">
+    <div className="flex flex-col min-h-screen items-center justify-center gap-4 px-6 py-12">
       <div className="my-auto mx-auto">
         <h1>What are we studying today?</h1>
         <p>Look up a bone, muscle, or structure you want to explore</p>
