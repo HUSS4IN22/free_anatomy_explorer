2a9b7c810fd1e917097bbce5a77f7bd8000b3769
Hussain <hussain.alialkhafaji567@gmail.com>
2026-04-21 23:01:16 -0400

    Develop structure of main search hero

--- frontend/src/blocks/MainSearch.tsx
+++ frontend/src/blocks/MainSearch.tsx
@@ -1,11 +1,13 @@
 import { InputGroup, InputGroupAddon, InputGroupButton, InputGroupInput } from "@/components/ui/input-group"
 
 export default function MainSearch() {
-  return <div className="flex flex-1 items-center justify-center max-w-2xl">
+  return <div>
+    <h1>What are we studying today?</h1>
+    <p>Look up a bone, muscle, or structure you want to explore</p>
     <InputGroup>
       <InputGroupAddon align="inline-start">icon</InputGroupAddon>
       <InputGroupInput placeholder="Search a body structure..." className="text-sm font-patrick" />
       <InputGroupButton>Enter</InputGroupButton>
     </InputGroup>
-  </div >
+  </div>
 }
