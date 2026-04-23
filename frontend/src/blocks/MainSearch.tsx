f2dd34f81eda63ce01acf76b10e7c2f4d73cd4b7
Hussain <hussain.alialkhafaji567@gmail.com>
2026-04-22 21:59:37 -0400

    Add some hero styling

--- frontend/src/blocks/MainSearch.tsx
+++ frontend/src/blocks/MainSearch.tsx
@@ -2,15 +2,19 @@ import { InputGroup, InputGroupAddon, InputGroupButton, InputGroupInput } from "
 
 export default function MainSearch() {
   return (
-    <div className="flex flex-col min-h-screen items-center justify-center gap-4 px-6 py-12">
-      <div className="my-auto mx-auto">
-        <h1>What are we studying today?</h1>
-        <p>Look up a bone, muscle, or structure you want to explore</p>
-        <InputGroup>
-          <InputGroupAddon align="inline-start">icon</InputGroupAddon>
-          <InputGroupInput placeholder="Search a body structure..." className="text-sm font-patrick" />
-          <InputGroupButton>Enter</InputGroupButton>
-        </InputGroup>
+    <div className="flex flex-col min-h-screen items-center justify-center gap-12 px-6 py-12">
+      <div>
+        <div className="text-2xl font-patrick text-center">
+          <h1>What are we studying today?</h1>
+          <p>Look up a bone, muscle, or structure you want to explore</p>
+        </div>
+        <div>
+          <InputGroup>
+            <InputGroupAddon align="inline-start">icon</InputGroupAddon>
+            <InputGroupInput placeholder="Search a body structure..." className="text-sm font-patrick" />
+            <InputGroupButton>Enter</InputGroupButton>
+          </InputGroup>
+        </div>
       </div>
     </div>
   )
