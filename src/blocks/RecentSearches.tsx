b65937df1149686728ed66968fbcf1887d468bb5
Hussain <hussain.alialkhafaji567@gmail.com>
2026-04-27 22:28:50 -0400

    Add Item component layout

--- src/blocks/RecentSearches.tsx
+++ src/blocks/RecentSearches.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button"
 import {
   Item,
   ItemActions,
@@ -8,5 +9,20 @@ import {
 } from "@/components/ui/item"
 
 export default function RecentSearches() {
-  return <div></div>
+  return (
+    <div>
+      <Item>
+        <ItemMedia>
+          Something
+        </ItemMedia>
+        <ItemContent>
+          <ItemTitle>Title</ItemTitle>
+          <ItemDescription>Description</ItemDescription>
+        </ItemContent>
+        <ItemActions>
+          <Button>Press</Button>
+        </ItemActions>
+      </Item>
+    </div>
+  )
 }
