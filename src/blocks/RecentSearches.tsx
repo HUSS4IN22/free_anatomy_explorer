b8ac05043eb06e8fb149b88a8e7d3250724d3e53
Hussain <hussain.alialkhafaji567@gmail.com>
2026-04-27 22:24:54 -0400

    Import Item component structure

--- src/blocks/RecentSearches.tsx
+++ src/blocks/RecentSearches.tsx
@@ -1,3 +1,12 @@
+import {
+  Item,
+  ItemActions,
+  ItemContent,
+  ItemDescription,
+  ItemMedia,
+  ItemTitle,
+} from "@/components/ui/item"
+
 export default function RecentSearches() {
   return <div></div>
 }
