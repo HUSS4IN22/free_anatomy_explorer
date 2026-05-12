bfec9e11fd8aa9b639af0fa421001e3ddd6b629a
Hussain <hussain.alialkhafaji567@gmail.com>
2026-05-11 23:36:49 -0400

    Add random Post API endpoint

--- backend/main.go
+++ backend/main.go
@@ -19,6 +19,9 @@ func main() {
 		return c.String(http.StatusOK, "I'm Saying Hi")
 	})
 
+	e.POST("/list", func(c *echo.Context) error {
+		return c.String(http.StatusOK, "1. hi 2. hello 3. yes")
+	})
 	if err := e.Start(":1323"); err != nil {
 		e.Logger.Error("failed to start server", "error", err)
 	}
