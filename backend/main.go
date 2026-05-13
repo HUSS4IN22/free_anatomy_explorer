dd4e8de19dd55e47a6b845d196a7e7e29683faeb
Hussain <hussain.alialkhafaji567@gmail.com>
2026-05-12 22:21:47 -0400

    Remove unnecessary API endpoint

--- backend/main.go
+++ backend/main.go
@@ -15,10 +15,6 @@ func main() {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
-	e.GET("/greet", func(c *echo.Context) error {
-		return c.String(http.StatusOK, "I'm Saying Hi")
-	})
-
 	if err := e.Start(":1323"); err != nil {
 		e.Logger.Error("failed to start server", "error", err)
 	}
